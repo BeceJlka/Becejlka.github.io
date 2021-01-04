7c0313f2dc54d8265dca0deffced61c290a8f62c
BeceJlka <59609028+BeceJlka@users.noreply.github.com>
2021-01-05 00:07:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_main():
+    return render_template('main.html')
+
+@app.route('/history/')
+def render_history():
+    return render_template('my_history.html')
+
+@app.route('/contact/')
+def render_contact():
+    return render_template("contact.html")
+
+@app.route('/work/')
+def render_work():
+    return render_template("work.html")
+
+if __name__ == '__main__':
+    app.run(port=8000,debug=False)
